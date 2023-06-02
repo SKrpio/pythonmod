7f3b8fd9a86176c5ab806df6de30fac4472dd050
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-06-03 09:33:56 +1000

    first session

---
+++ session_1/homework.py
@@ -0,0 +1,31 @@
+# three = "3"
+# nine = "9"
+# print(f"{three + nine}")
+
+# print(3+9)
+# print(-3+9)
+# print(3.0-9)
+# print(3*9)
+
+# distance = 1
+# other_distance = 5.4
+# print(type(distance))
+# print(f"1 kilometer is {distance*1000} meters")
+# print(f"5.4 kilometres is {other_distance*1000} meters")
+# print(type(other_distance))
+# print(f"{other_distance} kilometers is 5400 meters")
+
+# name = "Bob"
+# height = 283
+# print(f"What is your name? {name}")
+# print(f"How tall are you in meters? {height/100}")
+# print(f"{name} is {height}cms tall")
+
+# print("Hello human")
+# input("do you speak?")
+# strange_question = input ("What are you doing in my dimension?")
+# print(f"Well, then I shall watch you{strange_question}")
+
+name = input("What is your name?")
+height = input (f"How tall are you{name}?")
+print(f"{name} is{height}cms tall!")
---
+++ session_1/variables.py
@@ -0,0 +1,38 @@
+# Create a variable named first name and assign Sarah to it
+first_name = "Sarah"
+last_name = "Kernahan"
+
+# Data types - Text and Numerical data
+# Text data - String
+today = "Tuesday"
+# print(today)
+# print(f"today is {today}")
+# print(type(today))
+
+# Numerical data types - Integer, Float
+#Integer - whole number
+# parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+# height = 183
+# print(f"My height is {height/100} in m")
+
+#Float
+# weight = 60
+# print(type(weight))
+# print(f"My weight is {weight*2.1} in lbs")
+
+
+distance = "5000"
+# print(int(distance) + 8)
+# print (distance + str(8))
+
+print("Hello")
+input("Hello")
+dog_name = input ("What's the dogs name?")
+print(f"Nice to meet you {dog_name}")
+
+#Input function stores the data as string
+# birth_year = "What year were you born?"
+# print(f"You are {2023-int(birth_year)} years old") 
+
